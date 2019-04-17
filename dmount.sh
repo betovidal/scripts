fdf46633414802f1dc5421f963b5b7bd76e3d2bd
Gilberto Vidal <gilbertovidala@gmail.com>
2019-04-16 21:25:51 -0500

    Better mount

--- dmount.sh
+++ dmount.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
 # ask_for_permissions() {
 # }
+execute_command() {
+	cmd=$1
+	echo "Executing: \$$cmd"
+	$cmd
+}
 do_mount() {
-	for var in "$@"; do
-		echo "$var"
+	for disk in "$@"; do
+		ROUTE="/dev/$disk"
+		if [ -a $ROUTE ]; then
+			LABEL=$(ls -l /dev/disk/by-label | grep $disk | awk '{ print $9 }' )
+			if [ -z $LABEL ]; then
+				echo "Can't mount a disk without a label."
+				continue
+			fi
+			FOLDER="/media/$USER/$LABEL"
+			echo "Mounting : $ROUTE"
+			echo "Label    : $LABEL"
+			echo "User     : $USER"
+			echo "Folder   : $FOLDER"
+			if [ ! -d $FOLDER ]; then
+				echo "Need to create directory."
+				sudo mkdir $FOLDER
+			else
+				echo "Directory exists."
+			fi
+			if [[ $(findmnt -M "$FOLDER") ]]; then
+				echo "Route is in use by another mount!"
+			else
+				sudo mount $ROUTE $FOLDER
+			fi
+		else
+			echo "Can't find specified disk."
+		fi
 	done
 }
 ask_for_permissions() {
@@ -22,17 +52,12 @@ else
 fi
 # !!!!!!! END OF INNER BLOCK (If I indent this, it breaks)
 }
-execute_command() {
-	cmd=$1
-	echo "Executing: \$$cmd"
-	$cmd
-}
 if [ -z "$1" ]; then
 	execute_command "lsblk -o NAME,LABEL"
 	exit 1
 fi
 if sudo -n true; then
-	echo "You have permissions"
+	# echo "You have permissions"
 	do_mount $@
 else
 	ask_for_permissions $@
