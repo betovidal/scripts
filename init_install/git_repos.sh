1611fdff4cba51f70113ab34cb2f62d96469c450
Gilberto Vidal <gilbertovidala@gmail.com>
2019-06-09 09:34:36 -0500

    Fixed folder locations, added vundle

--- init_install/git_repos.sh
+++ init_install/git_repos.sh
@@ -1,23 +1,24 @@
 CONTAINER_FOLER=$HOME/Downloads/git_repositories
 VUNDLE_CONTAINER=$HOME/.vim/bundle/Vundle.vim
-if [ -d $CONTAINER_FOLER ]; then
+if [ ! -d $CONTAINER_FOLER ]; then
 	mkdir -p $CONTAINER_FOLER
 fi
-if [ -d $VUNDLE_CONTAINER ]; then
+if [ ! -d $VUNDLE_CONTAINER ]; then
 	mkdir -p $VUNDLE_CONTAINER
 fi
 cd $CONTAINER_FOLER
-git clone https://git.suckless.org/dmenu
-git clone https://git.suckless.org/dwm
-git clone https://git.suckless.org/farbfeld
-git clone https://git.suckless.org/sent
-git clone https://git.suckless.org/slock
-git clone https://git.suckless.org/slstatus
-git clone https://git.suckless.org/st
-git clone https://github.com/vim/vim.git
-git clone https://github.com/junegunn/fzf.git
-git clone https://github.com/ranger/ranger.git
+git clone https://git.suckless.org/dmenu $CONTAINER_FOLER/dmenu
+git clone https://git.suckless.org/dwm $CONTAINER_FOLER/dwm
+git clone https://git.suckless.org/farbfeld $CONTAINER_FOLER/farbfeld
+git clone https://git.suckless.org/sent $CONTAINER_FOLER/sent
+git clone https://git.suckless.org/slock $CONTAINER_FOLER/slock
+git clone https://git.suckless.org/slstatus $CONTAINER_FOLER/slstatus
+git clone https://git.suckless.org/st $CONTAINER_FOLER/st
+git clone https://github.com/vim/vim.git $CONTAINER_FOLER/vim
+git clone https://github.com/junegunn/fzf.git $CONTAINER_FOLER/fzf
+git clone https://github.com/ranger/ranger.git $CONTAINER_FOLER/ranger
 
-git clone https://github.com/betovidal/rcfiles.git
-git clone https://github.com/betovidal/scripts.git
+git clone https://github.com/betovidal/rcfiles.git $CONTAINER_FOLER/rcfiles
+git clone https://github.com/betovidal/scripts.git $CONTAINER_FOLER/scripts
 
+git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_CONTAINER
